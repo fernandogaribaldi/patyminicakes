f707b1782c391f4935133e3fda4add62325871af
fernandogaribaldi <fernando.garibaldi@gmail.com>
2025-08-05 17:22:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,31 @@ const imagens = [
 
 const container = document.getElementById('cardapio');
 
-imagens.forEach(nomeArquivo => {
+imagens.forEach(nome => {
+  const img = document.createElement('img');
+  img.src = nome;
+  img.alt = "Produto Paty Mini Cakes";
+  img.className = "imagem-card";
+
   const card = document.createElement('div');
   card.className = 'card';
-  card.innerHTML = `
-    <img src="${nomeArquivo}" alt="Produto Paty Mini Cakes" />
-  `;
+  card.appendChild(img);
   container.appendChild(card);
+
+  // Adiciona funcionalidade de clique
+  img.addEventListener('click', () => {
+    mostrarImagemModal(nome);
+  });
 });
 
+function mostrarImagemModal(imagemSrc) {
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modal-img');
+
+  modal.style.display = "flex";
+  modalImg.src = imagemSrc;
+}
+
+function fecharModal() {
+  document.getElementById('modal').style.display = "none";
+}
